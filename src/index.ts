b401cde3ef42335756771b703b0d45f0f01c8894
Łukasz Komar <8258799+lkomar@users.noreply.github.com>
2020-08-02 13:29:32 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express = require('express');
+import logger = require('morgan');
+import helmet = require('helmet');
+
+import { Request, Response } from 'express';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(helmet());
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).send({});
+});
+
+// start the Express server
+const port = app.get('port') || 3001;
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
