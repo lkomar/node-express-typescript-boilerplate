f3f7a32ddb76c3842a388e02fe3870a47347bee7
Łukasz Komar <8258799+lkomar@users.noreply.github.com>
2021-09-08 23:48:20 +0200

    Mandatory heroku deploy updates

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,10 @@ app.use(logger('dev'));
 app.use(helmet());
 
 app.get('/', (req: Request, res: Response) => {
-  res.status(200).send({});
+  res.status(200).send('Hello world!');
+  // res.status(200).send({});
 });
 
 // start the Express server
-const port = app.get('port') || 3001;
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
